ea90e1db7bdb6f8e3683172a51e08f116a07d7bb
Hamrouni-MohamedAmine <amineo2015@gmail.com>
2023-02-12 17:15:31 +0100

    Add project files.

---
+++ Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿using Builder;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var BMWBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+Console.WriteLine(miniBuilder.Car.ToString());
+
+garage.Construct(BMWBuilder);
+garage.Show();
+
+Console.ReadKey();
