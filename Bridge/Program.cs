be5ca464067c88a8dcd35175b3c34084464b53e0
Hamrouni-MohamedAmine <amineo2015@gmail.com>
2023-02-19 12:38:50 +0100

    Adapter, Bridge, Decorator

---
+++ Bridge/Program.cs
@@ -0,0 +1,20 @@
+﻿using Bridge;
+
+Console.Title = "Bridge";
+
+var noCoupon = new NoCoupon();
+var oneEuroCoupon = new OneEuroCoupon();
+
+var meatBasedMenu = new MeatBasedMenu(noCoupon);
+Console.WriteLine($@"Meat based Menu, no Coupon: {meatBasedMenu.CalculatePrice()} Euro.");
+
+meatBasedMenu = new MeatBasedMenu(oneEuroCoupon);
+Console.WriteLine($@"Meat based Menu, one euro Coupon: {meatBasedMenu.CalculatePrice()} Euro.");
+
+var vegetarienMenu = new VegetarienMenu(noCoupon);
+Console.WriteLine($@"Vegetarien Menu, no Coupon: {vegetarienMenu.CalculatePrice()} Euro.");
+
+vegetarienMenu = new VegetarienMenu(oneEuroCoupon);
+Console.WriteLine($@"Vegetarien Menu, one euro Coupon: {vegetarienMenu.CalculatePrice()} Euro.");
+
+Console.ReadLine();
