52f78c0c912366ecc7fc27c4957151cd12d1cebc
Hamrouni-MohamedAmine <amineo2015@gmail.com>
2023-02-21 12:03:43 +0100

    Facade

---
+++ Facade/Implementation.cs
@@ -0,0 +1,66 @@
+﻿namespace Facade
+{
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class OrderService
+    {
+        public bool HasEnoughOrders(int customerId)
+        {
+            // does the customer have enough orders? 
+            return(customerId > 5);
+        }
+    }
+
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class CustomerDiscountBaseService
+    {
+        public double CalculateDiscountBase(int customerId)
+        {
+            // fake calculation for demo purpose
+            return (customerId > 8) ? 10 : 20;
+        }
+    }
+
+    /// <summary>
+    /// Subsystem class
+    /// </summary>
+    public class DayOfTheWeekFactorService
+    {
+        public double CalculateDayOfTheWeekFactor()
+        {
+            // fake calculation for demo purposes
+            switch(DateTime.UtcNow.DayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Monday:
+                    return 0.8;
+                default:
+                    return 1.2;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Facade
+    /// </summary>
+    public class DiscountFacade
+    {
+        // only for demo purposes, otherwise we should use dependency injection,
+        private readonly OrderService _orderService = new();
+        private readonly CustomerDiscountBaseService _customerDiscountBaseService = new ();
+        private readonly DayOfTheWeekFactorService _dayOfTheWeekFactorService = new();
+
+        public double CalculateDiscountPercentage(int customerId)
+        {
+            if(!_orderService.HasEnoughOrders(customerId))
+            {
+                return 0;
+            }
+
+            return _customerDiscountBaseService.CalculateDiscountBase(customerId) * _dayOfTheWeekFactorService.CalculateDayOfTheWeekFactor();
+        }
+    }
+}
