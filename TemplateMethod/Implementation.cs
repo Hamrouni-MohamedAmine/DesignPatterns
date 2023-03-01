0bc3aeb03acd830c135eda4644ab4834c22eb8ac
Hamrouni-MohamedAmine <amineo2015@gmail.com>
2023-03-01 15:19:36 +0100

    Template Method

---
+++ TemplateMethod/Implementation.cs
@@ -0,0 +1,63 @@
+﻿namespace TemplateMethod
+{
+    /// <summary>
+    /// AbstractClass
+    /// </summary>
+    public abstract class MailParser
+    {
+        public virtual void FindServer()
+        {
+            Console.WriteLine("Finding server...");
+        }
+
+        public abstract void AuthenticateToServer();
+
+        public string ParseHTMLMailBody(string identifier)
+        {
+            Console.WriteLine("Parsing HTML mail body...");
+            return $@"This is the body of mail with id {identifier}";
+        }
+
+        /// <summary>
+        /// TemplateMethod
+        /// </summary>
+        /// <param name="identifier"></param>
+        /// <returns></returns>
+        public string ParseMailBody(string identifier)
+        {
+            Console.WriteLine("Parsing mail body (in template method)...");
+            FindServer();
+            AuthenticateToServer();
+            return ParseHTMLMailBody(identifier);
+        }
+    }
+
+    public class ExchangeMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Exchange");
+        }
+    }
+
+    public class ApacheMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Apache");
+        }
+    }
+
+    public class EudoraMailParser : MailParser
+    {
+        public override void FindServer()
+        {
+            Console.WriteLine("Finding Eudora server throught a custom algorithm...");
+        }
+
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Eudora");
+        }
+    }
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using TemplateMethod;
+
+Console.Title = "Template Method";
+
+ExchangeMailParser exchangeMailParser= new();
+Console.WriteLine(exchangeMailParser.ParseMailBody("123-456"));
+Console.WriteLine();
+
+ApacheMailParser apacheMailParser = new();
+Console.WriteLine(apacheMailParser.ParseMailBody("456-789"));
+Console.WriteLine();
+
+EudoraMailParser eudoraMailParser = new();
+Console.WriteLine(eudoraMailParser.ParseMailBody("789-123"));
+
+
+Console.ReadKey();
